d4d6c7d8b0a62ee334ae13e194caeb964f417dcc
Christian Grévisse <christian.grevisse@uni.lu>
2024-10-06 00:03:57 +0200

    Version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbank_genai';
-$plugin->release = '0.1.0';
+$plugin->release = '0.1.1';
 $plugin->version = 2024100501;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
